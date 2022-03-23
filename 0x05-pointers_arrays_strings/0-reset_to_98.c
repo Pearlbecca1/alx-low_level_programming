26d842a12b309ba8a98b9ab87fb85a659d0f6dd3
root <audurebecca25@gmail.com>
2022-03-22 17:32:56 -0700

    recreating 3-print_numbers.c file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,10 @@
 #include "main.h"
-/*
-* reset_to_98 - updates the value it points
-*@n: pointer of the direcction at the variable n
-*Return: 0 (success)
-*/
+/**
+ * reset_to_98 -a function that takes a pointer to an int
+ * @n: puntero
+ *
+ * Return: (0)
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
